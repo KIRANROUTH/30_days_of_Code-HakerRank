cc4c050a5e46de45aa296f83b98242e444dddbfd
Kiran babu <51410158+RouthKiranbabu@users.noreply.github.com>
2020-04-23 10:36:14 +0530

    Difficulty: Easy.

---
+++ Day 17 More Exceptions/Solution.py
@@ -0,0 +1,21 @@
+#Question Link: https://www.hackerrank.com/challenges/30-more-exceptions/problem
+
+class Calculator:
+    def __init__(self):
+        pass
+
+    def power(self, n, p):
+        if n < 0 or p < 0:
+            return "n and p should be non-negative"
+        else:
+            return pow(n, p)
+
+myCalculator=Calculator()
+T=int(input())
+for i in range(T):
+    n,p = map(int, input().split())
+    try:
+        ans=myCalculator.power(n,p)
+        print(ans)
+    except Exception as e:
+        print(e)   
